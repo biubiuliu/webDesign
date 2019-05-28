ff2ca0cd1814a6bdbc9c160813e581b834239c17
biubiuliu <38337309+biubiuliu@users.noreply.github.com>
2019-05-29 00:02:23 +0800

    素材库tabs

--- src/view/materialLib/materialLib.vue
+++ src/view/materialLib/materialLib.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="materialLib">
-        <h1 style="color:red">{{msg}}</h1>
+        <!-- <h1 style="color:red">{{msg}}</h1> -->
+        <div class="tag">
+            <Tabs value="name1" class="Tabs">
+                <TabPane label="公共库" name="name1">公共库1</TabPane>
+                <TabPane label="私人库" name="name2">私人库2</TabPane>
+            </Tabs>
+        </div>
     </div>
 </template>
 <script>
@@ -18,4 +24,14 @@ export default {
 .materialLib{
 
 }
+.tag{
+    width: 400px;
+   text-align: center;
+    height: auto;
+}
+
+.Tabs {
+    display: inline-block
+}
+
 </style>
